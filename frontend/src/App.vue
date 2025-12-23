365c182f6b385f4e1854deefc6e0316453b36d8d
Víctor J. <maximovj@outlook.com>
2025-12-22 20:31:39 -0600

    * Se agrega blur a las alertas

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <router-view />
-  <AlertCustom ref="alertRef" />
+  
+  <AlertCustom ref="alertRef" class="fixed inset-0 bg-black/40 backdrop-blur-sm z-[9999]" />
 
-<div
-  v-if="ui.loading"
-  class="fixed inset-0 bg-black/40 backdrop-blur-sm z-[9999] grid place-items-center"
->
-  <div class="flex flex-col items-center gap-3">
-    <ProgressSpinner
-      style="width:50px;height:50px"
-      strokeWidth="4"
-      animationDuration=".7s"
-    />
-    <span class="text-white font-semibold text-sm">
-      Cargando...
-    </span>
+  <div
+    v-if="ui.loading"
+    class="fixed inset-0 bg-black/40 backdrop-blur-sm z-[9999] grid place-items-center"
+  >
+    <div class="flex flex-col items-center gap-3">
+      <ProgressSpinner
+        style="width:50px;height:50px"
+        strokeWidth="4"
+        animationDuration=".7s"
+      />
+      <span class="text-white font-semibold text-sm">
+        Cargando...
+      </span>
+    </div>
   </div>
-</div>
-
 
 </template>
 
